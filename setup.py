869e561fd4954fac4af2879c57b0c7e20f07d52b
David Poirier-Quinot <davipoir@gmail.com>
2015-02-23 00:13:59 +0100

    modified setup.py to searc openhmd.h in /usr/local/include (for brew users)

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
- 
-module1 = Extension("rift", 
+
+module1 = Extension("rift",
     ["pyrift.pyx", "Rift.cpp"],
     language="c++",
     libraries=["openhmd"],
-    include_dirs=['/usr/include/openhmd'])
- 
+    include_dirs=['/usr/include/openhmd','/usr/local/include/openhmd'])
+
 setup(name = 'rift',
     version = '1.0',
     description = 'Python OpenHMD Wrapper',
